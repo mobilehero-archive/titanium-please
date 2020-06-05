7c77ca0caf65a8bd66833c8735de58f0819a891b
Brenton House <brenton.house@gmail.com>
2020-06-05 11:46:06 -0500

    update

--- Please.js
+++ Please.js
@@ -10,7 +10,8 @@ const UnauthorizedError = require('@titanium/errors/Unauthorized');
 console.debug(`🦠  [Please] process.env.DEBUG_MODE: ${process.env.DEBUG_MODE}`);
 
 let debug = () => {};
-if (process.env.DEBUG_MODE === 'true' || process.env.DEBUG_MODE === true) {
+if ((process.env.DEBUG_MODE === 'true') || (process.env.DEBUG_MODE === true)) {
+	console.debug('📌  you are here → DEBUG_MODE === true');
 	debug = (...args) => {
 		console.debug(...args);
 	};
